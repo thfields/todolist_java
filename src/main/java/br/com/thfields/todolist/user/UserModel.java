fefce6d0f8f7d5ff52ffb7e4a839d1df61e2931a
Thiago Campos <=>
2023-10-10 00:44:57 -0300

    aula01

--- src/main/java/br/com/thfields/todolist/user/UserModel.java
+++ src/main/java/br/com/thfields/todolist/user/UserModel.java
@@ -1,7 +1,7 @@
 package br.com.thfields.todolist.user;
 
 public class UserModel {
-    public String username;
+    private String username;
     public String name;
     private String password;
 }
