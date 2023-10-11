b0843cab56367a60d2e4d99a276bf0e16d2efe6f
Thiago Campos <=>
2023-10-10 23:52:16 -0300

    gerar gets e sets

--- src/main/java/br/com/thfields/todolist/user/UserModel.java
+++ src/main/java/br/com/thfields/todolist/user/UserModel.java
@@ -1,7 +1,31 @@
 package br.com.thfields.todolist.user;
 
 public class UserModel {
-    private String username;
-    private String name;
-    private String password;
+    public String username;
+    public String name;
+    public String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
