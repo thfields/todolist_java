1f3b8281c8ae4553ae611f11650caa14a5a79e1f
Thiago Campos <=>
2023-10-11 00:21:13 -0300

    gets e sets usando a lib lombok @Data e adicionando o database h2

--- src/main/java/br/com/thfields/todolist/user/UserModel.java
+++ src/main/java/br/com/thfields/todolist/user/UserModel.java
@@ -1,31 +1,12 @@
 package br.com.thfields.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
     public String username;
     public String name;
     public String password;
 
-    public String getUsername() {
-        return username;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
