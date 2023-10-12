b86853a674e894582180be460c9c1cfa5e0c1bdb
Thiago Campos <=>
2023-10-11 22:48:21 -0300

    criptografia de senha

--- src/main/java/br/com/thfields/todolist/user/UserController.java
+++ src/main/java/br/com/thfields/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.thfields.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +16,16 @@ public class UserController {
     @Autowired
     private IUserRepository userRepository;
     @PostMapping("/")
-    public ResponseEntity create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel){ //Criar usuário
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if(user != null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
         }
+
+        var passwordHashred =  BCrypt.withDefaults().hashToString(12,userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
