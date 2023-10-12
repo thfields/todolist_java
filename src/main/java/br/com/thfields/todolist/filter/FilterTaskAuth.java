bf651ffcacdca5377c3aaf2544b9e37037cb4df5
Thiago Campos <=>
2023-10-12 02:27:50 -0300

    avalidação de usuário e senha

--- src/main/java/br/com/thfields/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/thfields/todolist/filter/FilterTaskAuth.java
@@ -1,8 +1,11 @@
 package br.com.thfields.todolist.filter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.thfields.todolist.user.IUserRepository;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,10 +15,13 @@ import java.util.Base64;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
-
+        // Pegar a autenticação (usuário e senha)
         var authorization = request.getHeader("Authorization");
         var authEncoded = authorization.substring("Basic".length()).trim();
 
@@ -30,11 +36,25 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         String username = credentials[0];
         String password = credentials[1];
 
-        System.out.println("Authorization");
-        System.out.println(username);
-        System.out.println(password);
+        // Validar usuário
+            var user = this.userRepository.findByUsername(username);
+            if(user == null){
+                response.sendError(401);
+            }else {
+                // Validar senha
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(),user.getPassword());
+                if(passwordVerify.verified){
+                    chain.doFilter(request,response); // Continuar
+                }else {
+                    response.sendError(401);
+                }
 
-        chain.doFilter(request,response);
+
+
+
+            }
+
+
 
     }
 }
