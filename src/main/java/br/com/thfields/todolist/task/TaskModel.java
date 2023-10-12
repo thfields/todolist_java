d6e778a1fe374bff4ae7817aba976ab6bf02c8a5
Thiago Campos <=>
2023-10-11 23:04:34 -0300

    tabela de tarefas criada

---
+++ src/main/java/br/com/thfields/todolist/task/TaskModel.java
@@ -0,0 +1,45 @@
+package br.com.thfields.todolist.task;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/*
+ * id
+ * Usuário
+ * Descrição
+ * título
+ * data de inicio
+ * data de termino
+ * prioridade
+ * */
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+
+    @Column(length = 10)
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
