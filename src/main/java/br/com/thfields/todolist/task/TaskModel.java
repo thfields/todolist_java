5fa0cb12b7309c312565d9ca7894bf6741613b0c
Thiago Campos <=>
2023-10-13 22:17:10 -0300

    valexceptions error

--- src/main/java/br/com/thfields/todolist/task/TaskModel.java
+++ src/main/java/br/com/thfields/todolist/task/TaskModel.java
@@ -42,4 +42,11 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception{
+        if (title.length() > 50){
+            throw new Exception("50 caracteres no máximo");
+        }
+        this.title = title;
+    }
 }
